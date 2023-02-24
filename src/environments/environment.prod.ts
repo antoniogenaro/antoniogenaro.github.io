e2ad972d1a590aa3348571f1cc5e3ce4cd2127a4
Antonio Genaro <antoniogenaro8@gmail.com>
2023-02-25 00:26:20 +0100

    fix: endpoint remote

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   endpointRemote:
-    'https://raw.githubusercontent.com/antoniogenaro/antoniogenaro-web/master/src/assets/data',
+    'https://raw.githubusercontent.com/antoniogenaro/antoniogenaro.github.io/master/src/assets/data',
   endpointLocal: '/assets/data',
 };
